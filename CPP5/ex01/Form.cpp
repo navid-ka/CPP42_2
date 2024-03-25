b7209a3efdff3fae7731c1e2e3edfeb62707f512
Bifrost <nkeyani-@student.42barcelona.com>
2024-03-25 10:19:19 +0100

    chore: done

--- CPP5/ex01/Form.cpp
+++ CPP5/ex01/Form.cpp
@@ -15,22 +15,22 @@
 
 Form::Form(std::string name, int gradeSign, int gradeReqEx)
     : _name(name), _gradeSign(gradeSign), _gradeRequiredExec(gradeReqEx) {
-  try {
-    if (this->_name.empty())
-      throw std::invalid_argument("name cannot be empty");
-  } catch (std::invalid_argument &e) {
-    ONLYPRINTFANS("Exception in constructor name validation" << e.what());
-  }
-  _isGradeSigned = false;
+    try {
+        if (this->_name.empty())
+            throw std::invalid_argument("name cannot be empty");
+    } catch (std::invalid_argument &e) {
+        ONLYPRINTFANS("Exception in constructor name validation" << e.what());
+    }
+    _isGradeSigned = false;
 
-  try {
-    isGradeValid(_gradeSign);
-    isGradeValid(_gradeRequiredExec);
-  } catch (GradeTooHighException &err) {
-    ONLYPRINTFANS("Exception thrown in " << err.what());
-  } catch (GradeTooLowException &err) {
-    ONLYPRINTFANS("Exception thrown in " << err.what());
-  }
+    try {
+        isGradeValid(_gradeSign);
+        isGradeValid(_gradeRequiredExec);
+    } catch (GradeTooHighException &err) {
+        ONLYPRINTFANS("Exception thrown in " << err.what());
+    } catch (GradeTooLowException &err) {
+        ONLYPRINTFANS("Exception thrown in " << err.what());
+    }
 }
 
 Form::~Form() {}
@@ -40,9 +40,9 @@ Form::Form(const Form &copy)
       _gradeRequiredExec(copy._gradeRequiredExec) {}
 
 Form &Form::operator=(const Form &rhs) {
-  if (this != &rhs)
-    _isGradeSigned = rhs._isGradeSigned;
-  return (*this);
+    if (this != &rhs)
+        _isGradeSigned = rhs._isGradeSigned;
+    return (*this);
 }
 
 const std::string &Form::getName() const { return (_name); }
@@ -55,12 +55,11 @@ bool Form::getIsGradeSigned() const { return (_isGradeSigned); }
 
 void Form::beSigned(const Bureaucrat &bure) {
     if (bure.getGrade() <= _gradeSign) {
-      _isGradeSigned = true;
+        _isGradeSigned = true;
+    } else {
+        throw GradeTooLowException(
+            "The grade is not enough to sign the form\n");
     }
-    else {
-      throw GradeTooLowException("The grade is not enough to sign the form\n");
-    }
-
 }
 
 Form::GradeTooHighException::GradeTooHighException(const std::string &error)
@@ -70,16 +69,17 @@ Form::GradeTooLowException::GradeTooLowException(const std::string &error)
     : std::range_error(error) {}
 
 void Form::isGradeValid(int grade) {
-  if (grade < 1)
-    throw GradeTooHighException("Grade is too high");
-  if (grade > 150)
-    throw GradeTooLowException("Grade is too low");
+    if (grade < 1)
+        throw GradeTooHighException("Grade is too high");
+    if (grade > 150)
+        throw GradeTooLowException("Grade is too low");
 }
 
 std::ostream &operator<<(std::ostream &out, Form const &rhs) {
-  out << rhs.getName() << ", form signing grade " << rhs.getGradeSign() << ", Execution grade " << rhs.getGradeReq() << " is";
-  if (!rhs.getIsGradeSigned())
-    out << " NOT";
-  out << " signed" << std::endl;
-  return (out);
+    out << rhs.getName() << ", form signing grade " << rhs.getGradeSign()
+        << ", Execution grade " << rhs.getGradeReq() << " is";
+    if (!rhs.getIsGradeSigned())
+        out << " NOT";
+    out << " signed" << std::endl;
+    return (out);
 }
--- CPP5/ex01/main.cpp
+++ CPP5/ex01/main.cpp
@@ -8,50 +8,48 @@
 /*                              |___/                                 */
 /**********************************************************************/
 
-
-#include <iostream>
-#include <string>
 #include "Bureaucrat.hpp"
 #include "Form.hpp"
+#include <iostream>
+#include <string>
 
 int main(void) {
-	try {
-		Bureaucrat	bob("Bob", 2);
-		Bureaucrat	bill("Bill", 132);
+    try {
+        Bureaucrat bob("Bob", 2);
+        Bureaucrat bill("Bill", 132);
 
-		Bureaucrat & bill_ref = bill;
+        Bureaucrat &bill_ref = bill;
+        Form a("A13", 4, 6);
+        Form b("B97", 146, 135);
+        Form c("E01", 1, 150);
+        // Sign forms
+        std::cout << std::endl;
+        std::cout << "-------SIGN:-------- KO" << std::endl;
 
-		Form		a("A13", 4, 6);
-		Form		b("B97", 146, 135);
-		Form		c("E01", 1, 150);
-		// Sign forms
-		std::cout << std::endl;
-		std::cout << "-------SIGN:-------- KO" << std::endl;
+        // Sign KO
+        std::cout << "SIGN KO:" << std::endl;
+        bill_ref.signForm(a);
+        std::cout << a << std::endl;
+        std::cout << std::endl;
 
-		// Sign KO
-		std::cout << "SIGN KO:" << std::endl;
-		bill_ref.signForm(a);
-		std::cout << a << std::endl;
-		std::cout << std::endl;
+        std::cout << "SIGN KO:" << std::endl;
+        bob.signForm(c);
+        std::cout << c << std::endl;
+        std::cout << std::endl;
 
-		std::cout << "SIGN KO:" << std::endl;
-		bob.signForm(c);
-		std::cout << c << std::endl;
-		std::cout << std::endl;
+        std::cout << "-------SIGN:-------- KO" << std::endl;
+        // Sign OK
+        std::cout << "SIGN OK:" << std::endl;
+        bob.signForm(b);
+        std::cout << b << std::endl;
 
-		std::cout << "-------SIGN:-------- KO" << std::endl;
-		// Sign OK
-		std::cout << "SIGN OK:" << std::endl;
-		bob.signForm(b);
-		std::cout << b << std::endl;
+        // Sign OK
+        std::cout << "SIGN OK:" << std::endl;
+        bill.signForm(b);
+        std::cout << b << std::endl;
+    } catch (std::exception &err) {
+        std::cout << err.what() << std::endl;
+    }
 
-		// Sign OK
-		std::cout << "SIGN OK:" << std::endl;
-		bill.signForm(b);
-		std::cout << b << std::endl;
-	} catch (std::exception &err) {
-		std::cout << err.what() << std::endl;
-	}
-	
-  return (0);
+    return (0);
 }
