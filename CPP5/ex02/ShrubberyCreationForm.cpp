9395e78ab19f8be7cb20e6b7301e03a7ea04f272
Bifrost <nkeyani-@student.42barcelona.com>
2024-03-27 11:49:15 +0100

    chore: Schrubbery implementation done

--- CPP5/ex02/ShrubberyCreationForm.cpp
+++ CPP5/ex02/ShrubberyCreationForm.cpp
@@ -1,22 +1,45 @@
 #include "ShrubberyCreationForm.hpp"
 #include "AForm.hpp"
+#include <fstream>
 #include <iostream>
 
-ShrubberyCreationForm::ShrubberyCreationForm(const std::string &target)
-    : _target(target), AForm("ShrubberyCreationForm", 145, 137) {}
+ShrubberyCreationForm::ShrubberyCreationForm(std::string target)
+    : AForm("ShrubberyCreationForm", 145, 137) {
+    _target = target;
+    _file = target + "_shrubbery";
+}
 
 ShrubberyCreationForm::~ShrubberyCreationForm() {}
 
-ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm &cpy) {
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm &cpy)
+    : AForm(cpy.getName(), cpy.getGradeSign(), cpy.getGradeReq()) {
     _target = cpy._target;
+    _file = cpy._file;
 }
 
 ShrubberyCreationForm &
 ShrubberyCreationForm::operator=(const ShrubberyCreationForm &rhs) {
     if (this != &rhs) {
         _target = rhs._target;
+        _file = rhs._file;
+        AForm::operator=(rhs);
     }
     return (*this);
 }
 
-void ShrubberyCreationForm::executeForm() {}
+void ShrubberyCreationForm::executeForm() const {
+    std::ofstream out;
+    out.open(this->_file.c_str());
+    if (!out.is_open()) {
+        std::cerr << "Could not write to " << this->_file << std::endl;
+        out.close();
+    }
+    out << "    *\n";
+    out << "   ***\n";
+    out << "  *****\n";
+    out << " ********\n";
+    out << "***********\n";
+    out << "   %%\n";
+    out << "   %%\n";
+    out.close();
+}
--- CPP5/ex02/ShrubberyCreationForm.hpp
+++ CPP5/ex02/ShrubberyCreationForm.hpp
@@ -7,11 +7,13 @@
 class ShrubberyCreationForm : public AForm {
   private:
     std::string _target;
-    void executeForm() const;
+    std::string _file;
 
   public:
     ShrubberyCreationForm(std::string target);
     ~ShrubberyCreationForm();
     ShrubberyCreationForm(const ShrubberyCreationForm &cpy);
     ShrubberyCreationForm &operator=(const ShrubberyCreationForm &rhs);
+
+    void executeForm() const;
 };
